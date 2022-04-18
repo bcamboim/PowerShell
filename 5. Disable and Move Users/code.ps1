282069581230ef45ba93a7dadcbfaebc0cc29ad5
Beatriz Camboim <51267274+bcamboim@users.noreply.github.com>
2022-04-18 11:35:52 -0300

    Update
    Shorter Code

--- 5. Disable and Move Users/code.ps1
+++ 5. Disable and Move Users/code.ps1
@@ -12,21 +12,15 @@ foreach($user in $users){
         Get-ADUser -Identity $user.samaccountname -Properties * |Move-ADObject -TargetPath $OU
         Write-Host "User"$user.samaccountname"OU:"$OU
     
-    }
-
-    #Case 2: User is Enabled
-    if(Get-ADUser -Identity $user.samaccountname -Properties * |Where-Object {($_.Enabled -eq $TRUE)}){
+    }elseif(Get-ADUser -Identity $user.samaccountname -Properties * |Where-Object {($_.Enabled -eq $TRUE)}){ #Case 2: User is Enabled
 
         Write-Host "User"$user.samaccountname"is ENABLED" -BackgroundColor white -ForegroundColor DarkRed
         Write-Host "Disabling"$user.samaccountname"and moving to"$OU -BackgroundColor Black -ForegroundColor White
         Get-ADUser -Identity $user.samaccountname -Properties * |Set-ADUser -Enabled $False
         Get-ADUser -Identity $user.samaccountname -Properties * |Move-ADObject -TargetPath $OU
     
-    }
-
+    }elseif(Get-ADUser -Identity $user.samaccountname -Properties *|Where-Object {($_.Enabled -eq $FALSE) -and ($_.DistinguishedName -match $OU)}){
     #Case 3: User is Disabled and in the right OU, no action is required here. This part is just a validation!!
-    if(Get-ADUser -Identity $user.samaccountname -Properties *|Where-Object {($_.Enabled -eq $FALSE) -and ($_.DistinguishedName -match $OU)}){
-    
         Write-Host "User"$user.samaccountname"is DISABLED and in the RIGHT OU" -BackgroundColor Black -ForegroundColor White
 
     }
